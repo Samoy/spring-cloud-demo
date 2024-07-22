abe774023d788374a9166e69131ee77f205ba22e
Samoy <samoy_young@163.com>
2024-07-22 17:43:25 +0800

    fix: 修改报错

--- provider/src/main/java/com/samoy/ProviderApplication.java
+++ provider/src/main/java/com/samoy/ProviderApplication.java
@@ -1,11 +1,13 @@
 package com.samoy;
-import org.springframework.boot.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ProviderApplication {
     public static void main(String[] args) {
-       SpringBootApplication.run(ProviderApplication.class, args);
+        SpringApplication.run(ProviderApplication.class, args);
     }
 }
